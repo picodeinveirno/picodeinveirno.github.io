d95dbd4591376772085e225bb83acfc305070491
picodeinveirno <picodeinveirno@gmail.com>
2025-05-28 01:43:13 +0800

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,9 @@
+const season = "Summer";
+const year = 2000;
+const school = "The International School of Macao";
+const educator = "Ashton Song";
+const goverment_money = 8627;
+const years_elapsed = 0;
+
+
+export { season, year, school, educator, goverment_money, years_elapsed};
